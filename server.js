b38ba2341df2c5440c597abe60dde06fffe78bb1
eugenius123 <yugene123@gmail.com>
2018-08-21 12:46:32 -0400

    server set up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+
 const app = express();
 //db config
 const db = require('./config/keys').mongoURI;
@@ -10,6 +15,11 @@ mongoose.connect(db, { useNewUrlParser: true }).then(() => console.log('mongoDB 
 
 app.get('/', (req, res) => res.send('Hello'));
 
+//user routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Server running on port ${port}'));
+app.listen(port, () => console.log('Server running on port ${port} '));
