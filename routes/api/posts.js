92d56809c03ccf3e2f4099c7d3b701310a92bc16
eugenius123 <yugene123@gmail.com>
2018-09-05 15:19:17 -0400

    fixed ignore

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// @router GET api/users/test
+// @desc  Tests users route
+// @access  Public       
+
+router.get('/test', (req, res) => res.json({ msg: 'Posts Works'}));
+
+module.exports = router;
