c1c6536a4bb587e33daf3b6af2eb1ff2d89d046e
eugenius123 <yugene123@gmail.com>
2018-09-05 18:45:08 -0400

    error validation added for user log ing

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//load input validation
+const validateRegisterInput = require("../../validation/register");
+
 //load user model
 const User = require("../../models/User");
 
@@ -20,9 +23,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @access  Public
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email Already exists" });
+      errors.email = "Email Already Exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
