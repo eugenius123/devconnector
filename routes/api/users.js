2ed147b65f44bab59421067128818a3b77eb92f8
eugenius123 <yugene123@gmail.com>
2018-09-05 16:33:36 -0400

    login added

--- routes/api/users.js
+++ routes/api/users.js
@@ -21,7 +21,7 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email Already exists" });
     } else {
-      const avatar = garavtar.url(req.body.email, {
+      const avatar = gravatar.url(req.body.email, {
         s: "200", //size
         r: "pg", //rating
         d: "mm" //default
@@ -33,7 +33,43 @@ router.post("/register", (req, res) => {
         avatar,
         password: req.body.password
       });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
     }
+  });
+});
+
+// @router GET api/users/test
+// @desc  Login users route
+// @access  Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    //check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
   });
 });
 
